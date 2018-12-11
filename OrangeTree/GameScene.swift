fadab4fe08243ca4a4f80d27810fb34e05791180
Jayce Azua <jayce.azua@gmail.com>
2018-12-10 17:24:54 -0800

    launching oranges

--- OrangeTree/GameScene.swift
+++ OrangeTree/GameScene.swift
@@ -3,10 +3,18 @@ import SpriteKit
 class GameScene: SKScene {
     var orangeTree: SKSpriteNode!
     var orange: Orange?
+    var touchStart: CGPoint = .zero
+    var shapeNode = SKShapeNode()
     
     override func didMove(to view: SKView) {
         // connect game objects
         orangeTree = (childNode(withName: "tree") as! SKSpriteNode)
+        
+        // configure shapeNode
+        shapeNode.lineWidth = 20
+        shapeNode.lineCap = .round
+        shapeNode.strokeColor = UIColor(white: 1, alpha: 0.3)
+        addChild(shapeNode)
         
     }
     
@@ -14,18 +22,48 @@ class GameScene: SKScene {
         // Get the location of the touch on the screen
         let touch = touches.first!
         let location = touch.location(in: self)
+        
         // check if the touch was on the orange tree
         if atPoint(location).name == "tree" {
             // create the orange and add it to the scene at the touch location
             orange = Orange()
+            orange?.physicsBody?.isDynamic = false // don't nteract with physics
             orange?.position = location
             addChild(orange!)
-            // give the orange an impulse to make it fly!
-            let vector = CGVector(dx: 100, dy: 0)
-            orange?.physicsBody?.applyImpulse(vector)
+            // store the location of the touch
+            touchStart = location
         }
-
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // get the location of the touch
+        let touch = touches.first!
+        let location = touch.location(in: self)
         
+        // update the position of the Orange to the current location
+        orange?.position = location
+        
+        // Draw the firing vector
+        let path = UIBezierPath()
+        path.move(to: touchStart)
+        path.addLine(to: location)
+        shapeNode.path = path.cgPath
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Get the location of where the touch ended
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        // Get the difference between the start and end point as a vector
+        let dx = touchStart.x - location.x
+        let dy = touchStart.y - location.y
+        let vector = CGVector(dx: dx, dy: dy)
+        // Set the Orange dynamic again and apply the vector as an impulse
+        orange?.physicsBody?.isDynamic = true // interact with physics
+        orange?.physicsBody?.applyImpulse(vector)
+        
+        // remove the path from shapeNode
+        shapeNode.path = nil
     }
     
 }
